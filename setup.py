a57443853ae9e4be3de3625e8b8f8e79c25f2bf3
dsal3389 <dsal3389@gmail.com>
2018-12-09 00:28:33 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import re, io
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
 	name="easyTCP",
-	version='0.5.2',
+	version='0.5.3',
 	url='https://github.com/dsal3389/easyTCP',
 	download_url='https://github.com/dsal3389/easyTCP.git',
 	license='Apache 2.0',
@@ -15,24 +18,14 @@ setup(
 	install_requires=[
 		'cryptography==2.4.2'
 	],
+	packages=find_packages(),
+	long_description=long_description,
 	classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'License :: OSI Approved',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
 	]
 )
