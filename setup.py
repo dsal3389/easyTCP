073fb63021ed18815b6de16888a10002d81f42a7
dsal3389 <dsal3389@gmail.com>
2018-12-09 01:10:42 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as f:
 
 setup(
 	name="easyTCP",
-	version='0.5.6',
+	version='0.6.0',
 	url='https://github.com/dsal3389/easyTCP',
 	download_url='https://github.com/dsal3389/easyTCP.git',
-	license='Apache 2.0',
+	license='IMT',
 	author="Daniel Sonbolian",
 	author_email='dsal3389@gmail.com',
 	description="easy&fast way to create asyncronus server&clients",
@@ -29,7 +29,7 @@ setup(
 	long_description=long_description,
 	classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
